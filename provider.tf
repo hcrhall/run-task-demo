651233a89ef3f9b4bee4c424ecb30dd41dc77d1b
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2021-09-27 13:05:06 +1000

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
