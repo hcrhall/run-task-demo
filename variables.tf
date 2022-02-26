99fc915026af0dbb1237e991512be224df1dfdad
hcrhall <rhall@hashicorp.com>
2022-02-26 13:20:40 +1300

    Terraform fmt change

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "region" {
-    default = "us-east-1"
-    description = "The AWS region that the provider should use"
+  default     = "us-east-1"
+  description = "The AWS region that the provider should use"
 }
