dcbebbc20cbfaa2a47bbac1ee4b1f63214e5bfea
hcrhall <rhall@hashicorp.com>
2022-04-30 21:24:26 +1200

    Adding some magic

---
+++ aws_lambda_function.tf
@@ -0,0 +1,14 @@
+// This configuration will genenrate AWS resource for demonstration purposes
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello_world"
+  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  handler       = "exports.test"
+  runtime       = "nodejs12.x"
+  memory_size   = 1024
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+
+}
---
+++ demo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. ./demo-magic/demo-magic.sh
+
+# hide the evidence
+clear
+
+# Run Terraform Plan
+pe "terraform plan"
+
+# Change Configuration
+pe "code aws_lambda_function.tf"
+
+# Run Terraform Plan
+pe "terraform plan"
+
+# Git Commit
+pe "<>"
