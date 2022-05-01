2c3f48384f4fbb34d25be8463e27f089a6425f96
hcrhall <rhall@hashicorp.com>
2022-05-01 21:11:31 +1200

    Add GH pr create

--- demo.sh
+++ demo.sh
@@ -24,4 +24,11 @@ pe "git add ."
 pe "git commit -m 'Update AWS Lambda tracing configuration'"
 
 # Push the changes
-pe "git push origin main"
+pe "git push origin ${BRANCH}"
+
+# Create Pull Request
+pe "gh pr create \\
+--title 'Add Tracing Configuration to Lambda Function' \\
+--body 'This PR sets the tracing configuration mode on the hello_world AWS lambda function to 'PassThrough'' \\
+--label 'Enhancement' \\
+--project 'Run Task Demo'"
