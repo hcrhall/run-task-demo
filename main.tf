c84eefa4d75272a417d6da5d9442d66f1fe615c2
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2022-10-06 16:00:36 +1300

    Secure S3

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 // This configuration will genenrate AWS resource for demonstration purposes
 resource "aws_s3_bucket" "bucket" {
   bucket = "run-task-demo-bucket"
@@ -17,4 +22,16 @@ resource "aws_s3_bucket_public_access_block" "public" {
   block_public_acls       = true
   block_public_policy     = true
   restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
