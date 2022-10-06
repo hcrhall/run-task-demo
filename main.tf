c323fde67449640d19cc3e6bd37e700ce5fdaa3d
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2022-10-06 15:56:19 +1300

    Add ACL resource

--- main.tf
+++ main.tf
@@ -5,10 +5,16 @@ resource "aws_s3_bucket" "bucket" {
   //versioning {
   //  enabled    = true
   //}
-  acl = "private"
 
   tags = {
     Name        = "Public"
     Environment = "Production"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
