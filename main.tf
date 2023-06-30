fedb075363c640b6db1eb648d42def48760122fa
Ryan Hall <rhall@hashicorp.com>
2023-06-30 12:17:31 +1200

    Prisma Cloud Failure

--- main.tf
+++ main.tf
@@ -1,23 +1,25 @@
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
+# resource "aws_kms_key" "mykey" {
+#   description             = "This key is used to encrypt bucket objects"
+#   deletion_window_in_days = 10
+# }
 
 // This configuration will genenrate AWS resource for demonstration purposes
 resource "aws_s3_bucket" "bucket" {
   bucket = "run-task-demo-bucket"
+  acl    = "public-read"
 
-  // server_side_encryption_configuration {
-    // rule {
-    //  apply_server_side_encryption_by_default {
-    //    kms_master_key_id = aws_kms_key.mykey.arn
-    //    sse_algorithm     = "aws:kms"
-    //  }
-    // }
-  // }
-  //versioning {
-  //  enabled    = true
-  //}
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       kms_master_key_id = aws_kms_key.mykey.arn
+  #       sse_algorithm     = "aws:kms"
+  #     }
+  #   }
+  # }
+
+  # versioning {
+  #   enabled = true
+  # }
 
   tags = {
     Name        = "Public"
@@ -25,10 +27,10 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public" {
-  bucket                  = aws_s3_bucket.bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-  ignore_public_acls      = true
-}
+# resource "aws_s3_bucket_public_access_block" "public" {
+#   bucket                  = aws_s3_bucket.bucket.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   restrict_public_buckets = true
+#   ignore_public_acls      = true
+# }
