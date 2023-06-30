49af0a921c14e417b2a031a4533f30390d3d00e3
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2023-06-30 12:09:56 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ resource "aws_kms_key" "mykey" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "run-task-demo-bucket"
 
-  server_side_encryption_configuration {
+  // server_side_encryption_configuration {
     // rule {
     //  apply_server_side_encryption_by_default {
     //    kms_master_key_id = aws_kms_key.mykey.arn
     //    sse_algorithm     = "aws:kms"
     //  }
     // }
-  }
+  // }
   //versioning {
   //  enabled    = true
   //}
