ee0b9cb38126fbed8fae85df941f0d1cbccecd4d
Ryan Hall <36720823+hcrhall@users.noreply.github.com>
2024-04-03 13:00:02 +1300

    Mock response configuration

--- monkey/terraform.tf
+++ monkey/terraform.tf
@@ -15,8 +15,8 @@ terraform {
 
 provider "aws" {
   region = var.region
-  # skip_credentials_validation = true
-  # skip_requesting_account_id  = true
-  # access_key                  = "mock_access_key"
-  # secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
